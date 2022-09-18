ffbd4238e66f4742ecdc3c79f3f6c24bcfb8cbf4
phlahr <40176957+phlahr@users.noreply.github.com>
2022-09-18 19:12:19 +0200

    Update_schwerpunkt.js

--- _schwerpunkt.js
+++ _schwerpunkt.js
@@ -1,75 +1,50 @@
 
-/* anzahl, durchmesser */
+// class Xi {
+//     constructor(Bewehrung){
+//         this.Bewehrung = Bewehrung;
+//     };
+//     //Method - check if a diameter is given
+//     _check = (asds) => {
+//         return asds < 0 ? this._DsToAs(asds) : asds;
+//     }
+//     //Method - convert to As(cm²)value
+//     _DsToAs = (ds) => {
+//         return Math.round(Math.PI * Math.pow(Math.abs(ds)/2,2))/100
+//     }
+//     _round = (num) => {
+//         return +(Math.round(num + "e+3")+"e-3");
+//     }
+//     schwerpunkt = () => {
+//         let Asi = 0;  //Gesamtquerschnittsfläche
+//         let AsiXi = 0; //Gesamtquerschnittsfläche * Gesamtschwerpunkt
 
-class As {
-    constructor(a1, a2, a3, a4, a5, x1, x2, x3, x4, x5, ds1, ds2, ds3, ds4, ds5){
-
-        this.a1 = a1;
-        this.a2 = a2;
-        this.a3 = a3;
-        this.a4 = a4;
-        this.a5 = a5;
-
-        this.x1 = x1;
-        this.x2 = x2;
-        this.x3 = x3;
-        this.x4 = x4;
-        this.x5 = x5;
-
-        this.ds1 = ds1;
-        this.ds2 = ds2;
-        this.ds3 = ds3; 
-        this.ds4 = ds4;
-        this.ds5 = ds5;
-    };
+//         for (let i = 0; i < this.Bewehrung.length; i++){
+//             let _Asi =  this._check(this.Bewehrung[i].durchmesser) * this.Bewehrung[i].anzahl
+//             Asi = Asi + _Asi;
+//             AsiXi = AsiXi + _Asi * this.Bewehrung[i].schwerlage
+//         }
+//         return this._round(AsiXi/Asi);
+//     }
+// }
 
 
-    _check = (asds) => {
-        if(asds < 0){
-            return this._DsToAs(asds)
-        } else {
-            return (asds)
+class Xi {
+    constructor(a){this.a = a;};
+    _1 = (b) => {return b < 0 ? this._2(b) : b}
+    _2 = (c) => {return Math.round(Math.PI * Math.pow(Math.abs(c)/2,2))/100}
+    _3 = (d) => {return +(Math.round(d + "e+3")+"e-3")}
+    xi = () => {let x = 0;let xx = 0;
+        for (let i = 0; i < this.a.length; i++)
+        {let _x =  this._2(this.a[i].durchmesser) * this.a[i].anzahl;
+            x = x + _x;
+            xx = xx + _x * this.a[i].schwerlage;
         }
-    }
-
-    _DsToAs = (ds) => {
-        let as = Math.round(Math.PI * Math.pow(Math.abs(ds)/2,2))/100
-        return as;
-    }
-
-    _AsToDs = (as) => {
-        let ds = (Math.round((Math.sqrt(as/Math.PI))*100)/100)*2*10;
-        return ds;
-    }
-
-    schwerpunktZ = () => {
-        let As1 = this._check(this.ds1)*this.a1;
-        let As2 = this._check(this.ds2)*this.a2;
-        let As3 = this._check(this.ds3)*this.a3;
-        let As4 = this._check(this.ds4)*this.a4;
-        let As5 = this._check(this.ds5)*this.a5;
-
-        let As1x1 = As1*this.x1;
-        let As2x2 = As2*this.x2;
-        let As3x3 = As3*this.x3;
-        let As4x4 = As4*this.x4;
-        let As5x5 = As5*this.x5;
-
-        let Asi = As1 + As2 + As3 + As4 + As5;
-        let AsiXi = As1x1 + As2x2 + As3x3 + As4x4 + As5x5;
-
-        let xi = AsiXi/Asi;
-        console.log(xi)
-
+        return this._3(xx/x);
     }
 }
 
 
-
-let x = new As(4, 2, 0, 0, 0, 5.2, 6.7, 0, 0, 0, -20, -16, 0, 0, 0).schwerpunktZ();
-
-
-let Bewehrung = [
+let Asu = [
     {
         "anzahl" : 4,
         "schwerlage" : 5.2,
@@ -78,10 +53,22 @@ let Bewehrung = [
     {
         "anzahl" : 2,
         "schwerlage" : 6.7,
+        "durchmesser" : -20
+    },
+    {
+        "anzahl" : 2,
+        "schwerlage" : 6.7+2.5,
         "durchmesser" : -16
     }
 ]
 
+let xi = new Xi(Asu).xi()
 
-console.log(Bewehrung.length)
+console.log(xi)
 
+
+
+//_AsToDs = (as) => {
+//    let ds = (Math.round((Math.sqrt(as/Math.PI))*100)/100)*2*10;
+//    return ds;
+//}
